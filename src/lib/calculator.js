10097297e482df6ea034f1e3685e6b082f57a116
Robison Pereira Machado <robisonpmachado@gmail.com>
2022-07-28 21:17:22 -0300

    aula 4: query string

--- src/lib/calculator.js
+++ src/lib/calculator.js
@@ -1,4 +1,11 @@
 
 module.exports.sum = (num1, num2) => {
-    return parseInt(num1) + parseInt(num2);
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+
+    if(Number.isNaN(num1) || Number.isNaN(num2)){
+        throw new Error("Please check your input")
+    }
+
+    return num1 + num2;
 }
--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -10,4 +10,12 @@ it('should sum 2 and 2 even if one of them is a string and the result must be 4'
 
 it('should sum 2 and 2 even if one of them is a string and the result must be 4', () => {
     expect(() => sum('', 2)).toThrowError();
+
+    expect(() => sum([2, 2])).toThrowError();
+
+    expect(() => sum({})).toThrowError();
+
+    expect(() => sum()).toThrowError();
+
+
 });
