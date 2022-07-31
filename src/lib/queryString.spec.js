6051cf68d1483c28c1ba05d724bd136ba541b7c4
Robison Pereira Machado <robisonpmachado@gmail.com>
2022-07-31 16:30:07 -0300

    aula 6: refatorando com confiança

--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,13 +1,16 @@
-const {queryString} = require('./queryString');
-
+const { queryString } = require('./queryString');
 
 describe('Object to query string', () => {
-    it('should create a valid query string when a object is provided', () => {
-        const obj = {
-            name: 'Fabio',
-            profession: 'developer'
-        }
+  it('should create a valid query string when a object is provided', () => {
+    const obj = {
+      name: 'Fabio',
+      profession: 'developer',
+    };
 
-        expect(queryString(obj)).toEqual('name=Fabio&profession=developer')
-    });
+    expect(queryString(obj)).toEqual('name=Fabio&profession=developer');
+  });
 });
+
+/* describe('Query string to object', () => {
+
+}); */
